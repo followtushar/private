de2a428b1483e82f49eb44409410079c41206d13
Tushar Sharma <followtushar@gmail.com>
2021-11-25 16:57:40 +0000

    Update testsftp.sh

--- testsftp.sh
+++ testsftp.sh
@@ -6,32 +6,32 @@ user="SRCNTABI"
 date=$(date '+%Y%m%d')
 #date="20210531"
 sourcefolder="/usr/its/mms/outbox/ch/$date/*"
-targetfolder="/nta-sftp-management/management-scripts/test"
+targetfolder="/nta-sftp-management/management-scripts/test/"
 ipcopy=( "172.30.20.12" "172.30.20.18" )
 LOG="/NTABI/rsync.log"
 i=0
 #CODE
 
 
-if [[ ! -f $targetfolder/CONTROL-ITSCH-OPIEI-$date.success ]] ; then
-echo "Job has already run successfully today"
+if [[ -f $targetfolder/CONTROL-ITSCH-OPIEI-$date.success ]] ; then
+    echo "Job has already run successfully today"
 exit 0
 fi
 
-for i in "${ipcopy[@]}"
-do
-success="true"
-echo "Initial: $success"
-for file in "${files[@]}"; do
-echo $success
-echo "Checking files for file: $file"
-scp -rp $user@$i:$sourcefolder/$file $targetfolder
-error=$?
-if [ "$error" = "0" ] ; then
-echo "$file is copied with success" 
-elif [ "$error" = "1" ] ; then
-success="false"
-echo "$file does not exist"
-else
-success="false"
-
+for i in "${ipcopy[@]}"; do
+    success="true"
+    echo "Initial: $success"
+    for file in "${files[@]}"; do
+        echo $success
+        echo "Checking files for file: $file"
+        scp -rp $user@$i:$sourcefolder/$file $targetfolder
+        error=$?
+    done
+        if [ "$error" = "0" ] ; then
+        echo "$file is copied with success" 
+        elif [ "$error" = "1" ] ; then
+            success="false"
+        echo "$file does not exist"
+        else
+            success="false"
+done    
