953bfed414e8968a587dfcba48bc078bcdd1e4e4
Tushar Sharma <followtushar@gmail.com>
2021-11-26 11:14:45 +0000

    Update testsftp.sh

--- testsftp.sh
+++ testsftp.sh
@@ -4,21 +4,20 @@
 files=("T-TXN-TRANFC-*-ITSCH.DAT" "AT-TXN-TRANFC-*-ITSCH.DAT" "R-SPAD-PASS-*-OPIEI.DAT" "R-RTUT-ITSIS-*-OPIEI.DAT" "R-SAPPR-ITSCH-*-OPIEI.DAT" "R-SBPC-ITSCH-$ITSCH-*-OPIEI.DAT" "R-SETL-ITSCH-*-OPIEI.DAT" "R-SNGD-ITSCH-*-OPIEI.DAT" "R-TREC-ITSCH-*-OPIEI.DAT")
 user="SRCNTABI"
 date=$(date '+%Y%m%d')
-#date="20210531"
+date="20211118"
 sourcefolder="/usr/its/mms/outbox/ch/$date/*"
 targetfolder="/nta-sftp-management/management-scripts/test/"
-ipcopy=( "172.30.20.12" "172.30.20.18" )
+ipcopy= ("172.30.20.12" "172.30.20.18")
 LOG="/NTABI/rsync.log"
 i=0
 #CODE
 
 
-if [[ -f $targetfolder/CONTROL-ITSCH-OPIEI-$date.success ]] ; then
-    echo "Job has already run successfully today"
-exit 0
-fi
-
 for i in "${ipcopy[@]}"; do
+    if [[ ! -f $targetfolder/CONTROL-ITSCH-OPIEI-$date.success ]] ; then
+        echo "Job has already run successfully today"
+        exit 0
+    fi
     success="true"
     echo "Initial: $success"
     for file in "${files[@]}"; do
@@ -28,10 +27,19 @@ for i in "${ipcopy[@]}"; do
         error=$?
     done
         if [ "$error" = "0" ] ; then
-        echo "$file is copied with success" 
-        elif [ "$error" = "1" ] ; then
-            success="false"
+        echo "$file is copied with success"
+    elif [ "$error" = "1" ] ; then
+        success="false"
         echo "$file does not exist"
-        else
-            success="false"
-done    
+    else
+        success="false"
+    fi
+done 
+if [ $success = "true" ] ; then echo "success" > $targetfolder/CONTROL-ITSCH-OPIEI-$date.success
+#rm $targetfolder/CONTROL-ITSCH-OPIEI-$date.failed
+echo "All jobs run successfully" else
+#echo "failed" > $targetfolder/CONTROL-ITSCH-OPIEI-$date.failed
+echo "Some jobs did not complete successfully"
+fi
+
+
